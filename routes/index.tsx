7ccc4bce4589995b130b41cf5ed86d1932c1ed08
JuanFelipe3610 <jpena3610@gmail.com>
2023-02-17 18:04:05 -0500

    cambio el titulo

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,7 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import { listPosts } from "../utils/posts.ts";
 import { Post } from "../types.d.ts";
 import Button from "../islands/Button.tsx";
+import { asset } from "$fresh/runtime.ts";
 
 export const handler: Handlers = {
   async GET(req, context) {
@@ -15,7 +16,10 @@ export default function Home(props: PageProps) {
   const { posts } = props.data;
   return (
     <main class="p-4">
-      <h1 class="text-4xl font-blod">Mi blog</h1>
+      <div class="flex gap-[1rem]">
+        <img src={asset("./logo.svg")} alt="" />
+        <h1 class="text-4xl font-blod">Mi blog en deno</h1>
+      </div>
       <Button></Button>
       {posts.map((post: Post) => (
         <article class="p-4">
